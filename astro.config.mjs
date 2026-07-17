1b2fe2acfb714d06557c66be3cdc16cd17a6c75e
LunaBytes <luna@schleichy.de>
2026-07-17 11:09:32 +0200

    added cloudflare shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
 import mdx from "@astrojs/mdx";
 
@@ -13,4 +14,6 @@ export default defineConfig({
   },
 
   integrations: [mdx(), react()],
+  output: 'static',
+  adapter: cloudflare(),
 });
